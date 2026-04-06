86e8cdf143269bb12901ad326bd01b4349b53835
Lucas Romitti <romittikummerlucas@gmail.com>
2026-04-06 16:55:54 -0300

    Setup - Inicio Cadastro

---
+++ vue-lanchonete/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Cadastro from './components/Cadastro.vue';
+</script>
+
+<template>
+  <h1>AAAAAAAA</h1>
+  <Cadastro />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-lanchonete/src/components/Cadastro.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="cadastro">
+  <h1>Cadastro</h1>
+  <form action="">
+    <div class="name-item">
+        <label for="">Nome: </label>
+        <input type="text" placeholder="X-Burguer"> 
+    </div>
+    <div class="price-item">
+        <label for="">Preço: </label>
+        <input type="number" placeholder="99.99"> 
+    </div>
+    <div class="category-item">
+        <label for="">Categoria: </label>
+        <select name="category" id="category">
+            <option value="lanche">Lanche</option>
+            <option value="bebida">Bebida</option>
+            <option value="sobremesa">Sobremesa</option>
+        </select> 
+    </div>
+  </form>
+</div>
+</template>
+
+<style scoped>
+
+</style>
