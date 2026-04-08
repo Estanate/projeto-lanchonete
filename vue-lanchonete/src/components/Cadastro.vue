49dac85971bacee78a6e30a49b68301e80d93e01
Lucas Romitti <romittikummerlucas@gmail.com>
2026-04-08 16:59:20 -0300

    Delete/Resumo - Arrumar localItems

--- vue-lanchonete/src/components/Cadastro.vue
+++ vue-lanchonete/src/components/Cadastro.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { items } from '@/assets/Assets';
-import { reactive, ref } from 'vue';
+import { items } from '@/Data/Data';
+import { reactive } from 'vue';
 
 //cria o objeto do formulario, e dps coloca o v-model pra preencher as info
 const form = reactive({
@@ -38,14 +38,6 @@ function addItem() {
 
   <!-- Crio o formulario, chamando a função de add item quando o botao submit for clickado, nao dando reload na pagina -->
   <form @submit.prevent="addItem">
-    <div class="category-item">
-        <label for="">Categoria: </label>
-        <select v-model="form.category" required="">
-            <option value="Lanche">Lanche</option>
-            <option value="Bebida">Bebida</option>
-            <option value="Sobremesa">Sobremesa</option>
-        </select> 
-    </div>
     <div class="name-item">
         <label for="">Nome: </label>
         <input type="text" v-model="form.name" required=""> 
@@ -57,6 +49,14 @@ function addItem() {
         <div class="available-item">
         <label for="">Disponível: </label>
         <input type="checkbox" v-model="form.available"> 
+    </div>
+    <div class="category-item">
+        <label for="">Categoria: </label>
+        <select v-model="form.category" required="">
+            <option value="Lanche">Lanche</option>
+            <option value="Bebida">Bebida</option>
+            <option value="Sobremesa">Sobremesa</option>
+        </select> 
     </div>
     <div class="submit-item">
         <button type="submit">Adcionar ao Menu</button>
--- vue-lanchonete/src/components/Cardapio.vue
+++ vue-lanchonete/src/components/Cardapio.vue
@@ -1,32 +1,100 @@
 <script setup>
-import { items } from '../assets/Assets.js';
+import { items } from '../Data/Data.js';
+import { ref, computed } from 'vue';
+
+const localItems = ref([...items]);
+
+const filter = ref('Todos');
+const filteredItems = computed(() =>{
+    if (filter.value === 'Todos') {
+        return localItems.value;
+    }
+    else if (filter.value === 'Lanches') {
+        return localItems.value.filter(item => item.category === 'Lanche');
+    }
+    else if (filter.value === 'Bebidas') {
+        return localItems.value.filter(item => item.category === 'Bebida');
+    }
+    else if (filter.value === 'Sobremesas') {
+        return localItems.value.filter(item => item.category === 'Sobremesa');
+    }
+})
+
+const deleteItem = (id) => {
+    if(!confirm('Quer Excluir mesmo dog?')) return;
+
+    localItems.value = localItems.value.filter(item => item.id !== id)
+}
+
+const totalItems = computed(() => localItems.value.length);
+const availableItems = computed(() => {
+    return localItems.value.filter(item => item.available).length;
+});
+
 </script>
 
 <template>
-    <div class="cardapio">
-        <ul class="items-list">
-                <p class="cards"v-for="item in items" :key="item.id" :class="{indisponivel: !item.available}">
+
+    <!-- Filtros -->
+    <div class="filters">
+        <form>
+            <label class="filter-all">
+                <input type="radio" v-model="filter" value="Todos">
+                Todos
+            </label>
+            <label class="filter-lanches">
+                <input type="radio" v-model="filter" value="Lanches">
+                Lanches
+            </label>
+            <label class="filter-bebidas">
+                <input type="radio" v-model="filter" value="Bebidas">
+                Bebidas
+            </label>
+            <label class="filter-sobremesas">
+                <input type="radio" v-model="filter" value="Sobremesas">
+                Sobremesas
+            </label>
+        </form>
+    </div>
+
+    <!-- Resumos --
+     <div class="resumos">
+        <p>Total de Itens: {{ totalItems }}</p>
+        <p>Total disponivel: {{ availableItems }}</p>
+     </div>
+
+    <!-- lista de items -->
+    <ul class="items-list">
+            <p class="items" v-for="item in filteredItems" :key="item.id">
+                <div class="card"
+                :class="{indisponivel: !item.available}">
                     <span class="category">{{item.category}}</span>
                     <span class="name">{{ item.name }}</span>
                     <span class="price">R$: {{ item.price }}</span>
-                </p>
-        </ul>
-    </div>
+                    <button class="delete" @click="deleteItem(item.id)">Excluir</button>
+                </div>
+            </p>
+    </ul>
+
 </template>
 
 <style scoped>
-.indisponivel{
-    text-decoration: line-through;
-    color: gray;
-}
-
-.cards{
+.card{
     display: flex;
     flex-direction: column;
-    background-color: beige;
+    background-color: rgb(9, 157, 103);
+    color: white;
     border-radius: 5px;
     padding: 30px;
     margin-bottom:10px;
+}
+
+.indisponivel{
+    color: black;
+    background-color: rgb(215, 25, 18);
+    span{
+    text-decoration: line-through;
+    }
 }
 
 </style>
